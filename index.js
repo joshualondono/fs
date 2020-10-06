e3151f16c693c96c9d6603485e037c2fe493ae60
joshua londono <joshualondono@outlook.com>
2020-10-06 12:04:01 -0400

    add

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const mainFile = 'randomText.txt'
+
+
+const makeDir = fs.mkdir(path.join(__dirname, 'content'), (err) => {
+                if (err) {
+                    console.log(error(err));
+                }
+                console.log('content folder created');
+              });
+
+const makeFile = fs.appendFile(mainFile, 'Author: Joshua Londono', (err) => {
+                  if (err) {
+                    console.log(err);
+                  }
+                  else {
+                    console.log('randomtext.txt created');
+                  }
+                });
+
+const copyFile = fs.copyFile(mainFile, 'content', 'verbage.txt', (err) => {
+          if (err) {
+            console.log(err);
+          }
+          else {
+          console.log('verbage.txt created');
+           }
+                });
+
+  const delFile = setTimeout(function(){
+    fs.rmdir('content', () => {
+      console.log('deleted');
+
+    });
+  }, 6000);
+
+//console.log(data);
+makeDir;
+makeFile;
+copyFile;
+delFile;
